65783e6e01214e33f9d1c735e8ec97819353daea
julianboyne <julianboyne11@gmail.com>
2022-12-12 10:30:09 -0500

    sparkling wine

---
+++ src/data/data.js
@@ -0,0 +1,63 @@
+const sparklingItaly = [
+  {
+    name: "Prosecco Col dei Salici, Veneto DOC NV",
+    price: 44
+  },
+{
+  name: "Barone Pizzini Extra Brut Franciacorta DOGC",
+  price: 75
+},
+{
+  name: "Barone Pizzini Franciacorta DOGC",
+  price: 85
+}
+]
+
+const champagneFrance = [
+  {
+    name: "Ruinart Brut Rose, Champagne NV",
+    price: 195
+  },
+  {
+    name: "Ruinart Blanc de Blanc, Champagne NV",
+    price: 195
+  },
+  {
+    name: "Nicolas Feuillatte Brut Reserve 'Exclusive', Champagne NV",
+    price: 70
+  },
+  {
+    name: "Louis Roederer Champagne Collection 243",
+    price: 125
+  },
+  {
+    name: "Bollinger Special Cuvee",
+    price: 140
+  }
+]
+
+const rose = [
+  {
+    name: "Figuiere Magali, Cote de Provence",
+    price: 45
+  }
+]
+
+const pinotGrigio = [
+  {
+    name: "Lageder Pinot Grigio 'Terra Alpina', Alto Adige - Sud Tirol DOC 2021",
+    price: 42
+  },
+  {
+    name: "Tiefenbrunner Pinot Grigio, Alto Adige - Sud Tirol 2021 DOC",
+    price: 40
+  },
+  
+]
+
+export {
+  sparklingItaly,
+  champagneFrance,
+  rose,
+  pinotGrigio,
+}
