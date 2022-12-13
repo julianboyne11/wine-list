f2489e438113e3b91c0acf8507d721e2907a9d62
julianboyne <julianboyne11@gmail.com>
2022-12-13 10:13:55 -0500

    add red tuscany wine

--- src/data/data.js
+++ src/data/data.js
@@ -179,6 +179,143 @@ const redPiedmont = [
   }
 ]
 
+const barolo = [
+  {
+    name: "Damilano Cannubi, Barolo DOCG 2017",
+    price: 170
+  },
+  {
+    name: "Massolino, 'Serralunga' Barolo DOCG 2018",
+    price: 95
+  },
+  {
+    name: "Prunotto Bussia, Barolo DOCG 2018",
+    price: 100
+  },
+  {
+    name: "Cordero di Montezemolo Monfalletto DOCG 2018",
+    price: 85
+  },
+]
+
+const redSardinia = [
+  {
+    name: "Cantine Argiolas 'Turriga', Isola de Nuraghi IGt 2014",
+    price: 150
+  },
+]
+
+const redSicily = [
+  {
+    name: "Tasca d'Almerita Rosso del Conte 2015",
+    price: 100
+  },
+  {
+    name: "Donna Fugata, 'Mille e una Notte' Rosso, Sicilia IGT 2017",
+    price: 160
+  },
+]
+
+const redTuscany = [
+  {
+    name: "Casanova di Neri 'Tenuta Nuova', Brunello di Montalcino DOCG 2017",
+    price: 210
+  },
+  {
+    name: "Ciacci Piccolomini, Brunello di Montalcino DOCG 2017",
+    price: 120
+  },
+
+  {
+    name: "Argiano, Brunello di Montalcino DOCG 2017",
+    price: 99
+  },
+  {
+    name: "Castiglion Bosco, Brunello di Montalcino DOCG 2017",
+    price: 115
+  },
+  {
+    name: "Tenute del Cerro 'La Poderina', Brunello di Montalcino DOCG",
+    price: 105
+  },
+  {
+    name: "Jacopo Biondi Santi Castello di Montepo 'Sassoalloro' IGT",
+    price: 70
+  },
+  {
+    name: "Castello di Volpaia, Chianti Classico DOCG",
+    price: 58
+  },
+  {
+    name: "Cafaggio Chianti Classico DOCG 2019",
+    price: 44
+  },
+  {
+    name: "Badia a Coltibuono Chianti Classico  DOCG 2020",
+    price: 55
+  },
+  {
+    name: "Castello dei Rampolla Chianti Classico DOCG",
+    price: 75
+  },
+  {
+    name: "Famiglia Zingarelli Chianti Classico DOCG",
+    price: 40
+  },
+  {
+    name: "Masseto Toscanac IGT 2019",
+    price: 1600
+  },
+  {
+    name: "Ca Marcanda 'Promis' Bolgheri, Toscana DOC 2020",
+    price: 88
+  },
+  {
+    name: "Ca Marcanda 'Magari' Bolgheri, Toscana 2019",
+    price: 120
+  },
+  {
+    name: "Tenuta Antinori 'Guado sl Tasso', Bolgheri Superiore DOC 2019",
+    price: 210
+  },
+  {
+    name: "Ornellaia, 'Le Serre Nuove' Bolgheri Superiore DOC 2020",
+    price: 145
+  },
+  {
+    name: "Luce della Vite 'Luce' Toscana IGT 2020",
+    price: 180
+  },
+  {
+    name: "Casanova de Neri 'Pietradonice', Toscana IGT 2018",
+    price: 165
+  },
+  {
+    name: "Grattamacco Bolgheri Rosso, Toscana 2020",
+    price: 70
+  },
+  {
+    name: "Ornellaia 'Le Volte', IGT Toscana 2020",
+    price: 65
+  },
+  {
+    name: "Avignonesi Desiderio Merlot, Cotona 2018",
+    price: 120
+  },
+  {
+    name: "Il Poggione, Rosso di Montepulciano DOC 2020",
+    price: 72
+  },
+  {
+    name: "Avignonesi, Rosso di Montepulciano DOC 2019",
+    price: 45
+  },
+  {
+    name: "Avignonesi, Vino Nobile di Montepulciano DOCG 2017",
+    price: 55
+  },
+]
+
 
 export {
   sparklingItaly,
@@ -197,4 +334,8 @@ export {
   whiteCalifornia,
   redApulia,
   redPiedmont,
+  barolo,
+  redSardinia,
+  redSicily,
+  redTuscany,
 }
